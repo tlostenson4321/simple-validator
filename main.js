7bac5ec573ba6a0f35991df8164eb5a60432c5bf
Student <student@RU-019.local>
2015-09-08 11:51:42 -0600

    second done

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-var phoneNumber = prompt ( "Please enter your phone number" +"\n" + "ex. 303-555-0123" +"\n" + "This will not be used for spam")
+var phoneNumber = prompt ( "Please enter your phone number" +"\n" + "ex. xxx-xxx-xxxx" +"\n" + "This will not be used for spam")
 console.log(phoneNumber)
 
 if (phoneNumber.charAt(3) === "-" && phoneNumber.charAt(7) === "-") {
 	alert ("true")
 }
 else {
-	alert ("false")
+	alert ("false\nPlease use format xxx-xxx-xxxx")
 }
 
 var birthDate = prompt ("Please enter your birth date" +"\n" + "ex. xx/xx/xx")
+console.log(birthDate)
+
+if (birthDate.charAt(2) === "/" && birthDate.charAt(5) === "/") {
+	alert ("true")
+}
+else {
+	alert ("false\nPlease use format xx/xx/xx")
+}
